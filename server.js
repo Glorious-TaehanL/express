56985ad65051b39c506f34862995c7b591207c09
System Administrator <root@Teds-MacBook-Pro.local>
2023-03-10 16:01:14 +0900

    update init test example

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+// app.use(cors());
+app.listen(8080, function () {
+  console.log('linstening on 8080');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/beauty', function (req, res) {
+  res.json({ 'beauty content': 'blabla~' });
+});
+app.get('/user/:id', function (req, res) {
+  const { id } = req.params;
+  console.log('id :: ' + id);
+
+  if (id == 'ted') {
+    res.json({ 'beauty content': id });
+  } else {
+    res.json({ 'beauty content': 'none' });
+  }
+});
